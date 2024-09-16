1661cd6d73aec82a6cffdfd3df496f5058dd1863
Dirag Biswas <hey@diragb.dev>
2024-09-16 14:23:37 +0530

    feat: firebase setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  reactStrictMode: false,
+  compiler: {
+    styledComponents: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
